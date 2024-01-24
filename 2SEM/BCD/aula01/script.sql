5d62c40b9533f6f9abd15e15a7a85226450b0e18
BuenoOtavio <buenootaviolima@gmail.com>
2024-01-24 08:05:54 -0300

    2024 uhuuuu

---
+++ 2SEM/BCD/aula01/script.sql
@@ -0,0 +1,22 @@
+create database lojinha;
+show databases;
+use lojinha;
+drop database lojinha;
+
+-- c:
+
+-- cd \
+
+-- cd xampp
+
+-- cd mysql
+
+-- cd bin
+
+-- mysql -u root
+
+-- create database lojinha;
+
+-- show databases
+
+-- exit
