2e5fa9318a156b69390c63b2fb321c45566548ea
BuenoOtavio <buenootaviolima@gmail.com>
2024-03-13 11:28:47 -0300

    Update 13/03/24

--- 2SEM/BCD/atv01/script.sql
+++ 2SEM/BCD/atv01/script.sql
@@ -18,3 +18,12 @@ select * from rota order by distancia;
 select * from funcionario order by nome;
 
 --6
+select placa, count(idEntrega)  as 'total de entregas' FROM entrega group by placa order by count (idEntrega) desc limit 1;
+
+--7
+create view relatorio_de_entregas_01 as
+select idEntrega, placa, motorista from entrega;
+
+--8
+create view relatorio_de_entregas_02 as
+Select e.motorista, e.placa, sum(p.valor) as 'Valor Entrega' from entrega e inner join pedido p on e.idEntrega = p.idEntrega group by p.idEntrega;
