459de26c2a41be8f8e5b2eee71e0a5611104cb52
BuenoOtavio <buenootaviolima@gmail.com>
2024-03-26 11:30:29 -0300

    Update

--- 2SEM/BCD/ProvaFormativa/Desafio05/script.sql
+++ 2SEM/BCD/ProvaFormativa/Desafio05/script.sql
@@ -9,3 +9,9 @@ select * from veiculos where marca = 'Fiat' order by ano desc;
 
 --Crie uma consulta que liste os funcionários em ordem alfabética de nome
 select v.*, COUNT(m.idManutencao) as qtdManutencoes from veiculos v left join manutencao m on v.placa = m.placaVeiculo group by v.placa;
+
+CREATE VIEW tdsManutencoes AS
+SELECT m.*, f.nome AS nomeFuncionario, v.modelo AS modeloVeiculo
+FROM manutencao m
+INNER JOIN funcionario f ON m.matriculaFuncionario = f.matricula
+INNER JOIN veiculos v ON m.placaVeiculo = v.placa;
