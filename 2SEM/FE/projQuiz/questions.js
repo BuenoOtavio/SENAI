ba212208a2e479a864d0198a375899363884e153
BuenoOtavio <buenootaviolima@gmail.com>
2024-05-15 11:28:44 -0300

    Update

---
+++ 2SEM/FE/projQuiz/questions.js
@@ -0,0 +1,37 @@
+export default[
+    {
+        question: "Quanto é 1 + 2",
+        answers: [
+            {option: "3", correct:true},
+            {option: "4", correct:false},
+            {option: "5", correct:false},
+        ],
+    },
+
+    {
+        question: "Quanto é 5 x 2",
+        answers: [
+            {option: "7", correct:false},
+            {option: "10", correct:true},
+            {option: "25", correct:false},
+        ],
+    },
+
+    {
+        question: "Quado Michael jackson Nasceu? ",
+        answers: [
+            {option: "1961", correct:false},
+            {option: "1955", correct:false},
+            {option: "1958", correct:true},
+        ],
+    },
+
+    {
+        question: "Quantos GRAMMYS Michael Jackson Tem? ",
+        answers: [
+            {option: "17", correct:false},
+            {option: "9", correct:false},
+            {option: "13", correct:true},
+        ],
+    },
+];
